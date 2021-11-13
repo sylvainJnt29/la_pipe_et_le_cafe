cdb50b3f83368e6d3d6a5e0d488be50f0ea45924
sylvainJnt29 <sylvain.jnt@gmail.com>
2021-11-13 16:19:38 +0100

    animation pipe + cafe ok

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,7 @@ import './bootstrap';
 // Import de gsap (manuel après installation npm)
 import { gsap } from "gsap";
 import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin.js';
 
 
 
@@ -80,7 +81,46 @@ gsap.fromTo("#grue",{
 
 // Animations articles page Pipe + Café
 
+// gsap.fromTo("#test", {
+//   scrollTrigger: "#test", // start the animation when ".box" enters the viewport (once)
+//   x: -2000
+//   },{
+//       x:5,
+//       duration: 2
+//   }
+// );
 
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+    var test = gsap.timeline({
+        scrollTrigger: {
+        trigger: "#test",
+        toggleActions: "play pause play reverse"
+        }
+        });
 
+    test.from("#test", {x:-1800,rotationX:-360,duration:2.2,ease:"power2"})
+    .to("#test", {x:0});
+
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+    var test2 = gsap.timeline({
+        scrollTrigger: {
+        trigger: "#test2",
+        toggleActions: "play pause play reverse"
+        }
+        });
+
+    test2.from("#test2", {rotationX:-360,duration:2.2,ease:"power2"})
+    .to("#test2", {x:0});
+
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+    var test3 = gsap.timeline({
+        scrollTrigger: {
+        trigger: "#test3",
+        toggleActions: "play pause play reverse"
+        }
+        });
+
+    test3.from("#test3", {x:1800,rotationX:-360,duration:2.2,ease:"power2"})
+    .to("#test3", {x:0});
 
 // Fin animations articles page Pipe + Café
